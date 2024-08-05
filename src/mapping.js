7f33f7500aa5d266fa6bff8c00bdb6d4d34dc20c
Nimsara Liyanage <tharindanimsara457@gmail.com>
2024-08-05 13:11:02 +0545

    mapping.js

---
+++ src/mapping.js
@@ -0,0 +1,14 @@
+const sinhalaToRoman = {
+    'අ': 'a', 'ආ': 'aa', 'ඇ': 'ae', 'ඈ': 'aae', 'ඉ': 'i', 'ඊ': 'ii', 'උ': 'u', 'ඌ': 'uu',
+    'ඍ': 'ru', 'ඎ': 'roo', 'ඏ': 'lu', 'ඐ': 'loo', 'එ': 'e', 'ඒ': 'ee', 'ඓ': 'ai', 'ඔ': 'o',
+    'ඕ': 'oo', 'ඖ': 'au', 'ක': 'ka', 'ඛ': 'kha', 'ග': 'ga', 'ඝ': 'gha', 'ඞ': 'nga', 'ඟ': 'nnga',
+    'ච': 'ca', 'ඡ': 'cha', 'ජ': 'ja', 'ඣ': 'jha', 'ඤ': 'nya', 'ඥ': 'gnya', 'ඦ': 'jnja',
+    'ට': 'ta', 'ඨ': 'tha', 'ඩ': 'da', 'ඪ': 'dha', 'ණ': 'na', 'ඬ': 'nda', 'ත': 'tha', 'ථ': 'ttha',
+    'ද': 'da', 'ධ': 'dha', 'න': 'na', 'ඳ': 'nda', 'ප': 'pa', 'ඵ': 'pha', 'බ': 'ba', 'භ': 'bha',
+    'ම': 'ma', 'ඹ': 'mba', 'ය': 'ya', 'ර': 'ra', 'ල': 'la', 'ව': 'va', 'ශ': 'sha', 'ෂ': 'sha',
+    'ස': 'sa', 'හ': 'ha', 'ළ': 'lla', 'ෆ': 'fa', 'ක්‍ෂ': 'ksha', 'ජ්‍ඤ': 'gnya'
+    // Add more mappings as needed
+  };
+  
+  export default sinhalaToRoman;
+  
